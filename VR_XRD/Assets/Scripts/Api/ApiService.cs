109688dbd9101bc16ae5a188d3913a6f1e414c05
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-11-10 18:55:35 +0100

    Ved ikke, mit unity crasher hver anden gang, så prøvet lige at fjerne nogle filer jeg havde lavet

--- VR_XRD/Assets/Scripts/Api/ApiService.cs
+++ VR_XRD/Assets/Scripts/Api/ApiService.cs
@@ -4,54 +4,58 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Meta.WitAi.Json;
+
+
 [UnityEngine.Scripting.Preserve]
 public class ApiService : MonoBehaviour
 {
     private string apiUrl;
     public string authToken;
 
-    public async Task<MachineData> GetMachineData()
+    private async void Start()
+    {
+        var machineData = await GetMachineData();
+        // Handle machineData if necessary
+    }
+
+    private async Task<MachineData> GetMachineData()
     {
         apiUrl = "http://localhost:5000/getMachine?id=1";
-        try
+        using (HttpClient client = new HttpClient())
         {
-            Debug.Log("Api Start");
-            using (HttpClient client = new HttpClient())
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+            try
             {
-                //TODO Måske overvej en bedre måde at indsætte en token på, men tænker det er for meget arbejde og skulle logge ind først
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
-
                 HttpResponseMessage response = await client.GetAsync(apiUrl);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    Debug.Log("Api SuccessS ");
                     string responseData = await response.Content.ReadAsStringAsync();
+                    Debug.Log(responseData);
                     MachineData machineData = JsonConvert.DeserializeObject<MachineData>(responseData);
-                    TestApi(machineData);
+                    TestApi(machineData); 
                     Debug.Log(machineData.ToString());
                     return machineData;
-                    
                 }
                 else
                 {
-                    Debug.LogError("Fejl i API-anmodning: " + response.ReasonPhrase);
+                    Debug.LogError("API Request Error: " + response.ReasonPhrase);
                     return null;
                 }
             }
+            catch (Exception ex)
+            {
+                Debug.LogError("API Request Exception: " + ex.Message);
+                return null;
+            }
         }
-        catch (Exception ex)
-        {
-            Debug.LogError("Fejl i API-anmodning: " + ex.Message);
-            return null;
-        }
-        
     }
-    //TODO Skal slettes senere når vi ved det virker
-    void TestApi(MachineData machineData)
+    
+    // Consider removing after testing
+    private void TestApi(MachineData machineData)
     {
-        Debug.Log("Maskinens navn: " + machineData.machineName);
-        Debug.Log("Beskrivelse: " + machineData.description);
+        Debug.Log("Machine Name: " + machineData.machineName);
+        Debug.Log("Description: " + machineData.description);
         Debug.Log("Status: " + machineData.statusDescription);
     }
 }
