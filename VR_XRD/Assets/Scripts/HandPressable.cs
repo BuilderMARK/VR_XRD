aa61176bf2cd53ddd77a09f2f33036f7dfbb44e6
Andre <andreas79001@gmail.com>
2023-11-11 22:29:27 +0100

    Added start/stop logic + scripts + scene update + linking it to work
    Should be working - not tested... No vr

---
+++ VR_XRD/Assets/Scripts/HandPressable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using OculusSampleFramework;
+
+public class HandPressable : MonoBehaviour
+{
+    public OVRHand hand;
+    public OVRInput.Controller controller;
+    public float distanceThreshold = 0.1f;
+
+    public ProductionLinker productionLinker; //Maybe rewrite this logic? SKal bare bruges til at gøre x når man trykker
+    public bool isStart; //Bare den funktion der skal kører på scriptet over... Ie. samme handpressable script til begge start stop
+
+    void Update()
+    {
+        Vector3 handPosition = hand.transform.position;
+        Vector3 buttonPosition = this.transform.position;
+
+        if (Vector3.Distance(handPosition, buttonPosition) < distanceThreshold)
+        {
+            Debug.Log("Button pressed");
+            if (isStart)
+            {
+                productionLinker.ToggleRunning(true);
+            } else
+            {
+                productionLinker.ToggleRunning(false);
+            }
+        }
+    }
+}
